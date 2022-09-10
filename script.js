bb29f397ab51b89726edbcdd7df1b6107bef9b3e
Sam White <samuelwhite98@outlook.com>
2022-09-10 11:00:54 +0100

    Add functionality to add books to library

--- script.js
+++ script.js
@@ -11,13 +11,20 @@ function Book(title,author,pages,read){
     };
 };
 
-function addBookToLibrary(title,author,pages,read){
-    newBook = new Book(title,author,pages,read)
+function addBookToLibrary(){
+    const form = document.querySelector(".popup-form")
+    form.classList.remove("form-open")
+    var title = document.querySelector("#title")
+    var author = document.querySelector("#author")
+    var pages = document.querySelector("#pages")
+    newBook = new Book(title.value,author.value,pages.value,read.value)
     myLibrary.push(newBook)
+    displayLibrary(myLibrary)
 }
 
 function displayLibrary(myLibrary){
     const DOMLibrary = document.querySelector(".library")
+    clearLibrary(DOMLibrary)
     myLibrary.forEach( book => {
         var bookContent = [book.title,book.author, book.pages, book.read]
         var DOMBook = document.createElement('div')
@@ -27,6 +34,22 @@ function displayLibrary(myLibrary){
     });
 }
 
-addBookToLibrary("book1","author1",100,"yes")
-addBookToLibrary("book2","author2",200,"yes")
-displayLibrary(myLibrary)
+function clearLibrary(DOMLibrary){
+    while (DOMLibrary.hasChildNodes()) {
+        DOMLibrary.removeChild(DOMLibrary.lastChild)
+      }
+}
+
+function openForm(){
+    const form = document.querySelector(".popup-form")
+    form.classList.add("form-open")
+}
+
+
+
+addButton = document.querySelector(".add-button")
+addButton.addEventListener("click", openForm)
+submitButton = document.querySelector("#submit")
+submitButton.addEventListener("click", addBookToLibrary)
+
+
